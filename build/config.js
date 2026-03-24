fe5544327e9bd5255fc629502c7084868a60fd59
immanu-el-06 <eljoeldho06@gmail.com>
2026-03-23 23:45:56 -0500

    Remove build folder

--- build/config.js
+++
@@ -1,3 +0,0 @@
-const DEV = false; // flip to false for production builds
-const API_BASE_URL = DEV ? "http://localhost:8000" : "https://traker-api.azurewebsites.net";
-const WEB_APP_URL = DEV ? "http://localhost:5173" : "https://traker.azurewebsites.net";
